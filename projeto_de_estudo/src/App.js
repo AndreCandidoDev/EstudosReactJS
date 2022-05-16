c35f62a7c091a3a8e888db385c12626b31517ba1
andrec762 <andd68@hotmail.com>
2022-05-16 19:11:42 -0300

    componentes de classe com estado

--- projeto_de_estudo/src/App.js
+++ projeto_de_estudo/src/App.js
@@ -1,25 +1,58 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  
+    state={
+      name: 'alguem',
+      counter:0
+    }
+
+    lixo='teste'
+
+    //passa atributo this para a função handlePClick
+    handlePCLick = this.handlePCLick.bind(this) //usar para passar qualquer atributo this para funções
+
+  handlePCLick(){
+    //const {name} = this.state
+    //console.log(this.lixo)
+    //console.log(`Foi clicado ${name}`)
+    this.setState({name:"mudou nome"})
+  }
+
+  //com arrow function o this vem nativamente na função não precisando dar bind
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({counter:counter+1})
+  }
+
+  render(){
+    //const name = this.state.name
+    const { name, counter } = this.state //Atribuição via desestruturação
+   
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePCLick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            add counter
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
