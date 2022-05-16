7e06a940be6023e8363d4e24ea4a54ff3ae2fe19
andrec762 <andd68@hotmail.com>
2022-05-16 19:29:24 -0300

    estado com array e objetos

--- projeto_de_estudo/src/App.js
+++ projeto_de_estudo/src/App.js
@@ -1,55 +1,43 @@
 import React from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component{
 
   
-    state={
-      name: 'alguem',
-      counter:0
-    }
-
-    lixo='teste'
-
-    //passa atributo this para a função handlePClick
-    handlePCLick = this.handlePCLick.bind(this) //usar para passar qualquer atributo this para funções
-
-  handlePCLick(){
-    //const {name} = this.state
-    //console.log(this.lixo)
-    //console.log(`Foi clicado ${name}`)
-    this.setState({name:"mudou nome"})
-  }
-
-  //com arrow function o this vem nativamente na função não precisando dar bind
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter:counter+1})
+  state={
+    posts: [
+      {
+        'id':1,
+        'title': 'titulo 1',
+        'body': 'corpo 1'
+      },
+      {
+        'id':2,
+        'title': 'titulo 2',
+        'body': 'corpo 2'
+      },
+      {
+        'id':3,
+        'title': 'titulo 3',
+        'body': 'corpo 3'
+      },
+    ],
+    name: 'alguem',
+    counter:0
   }
 
   render(){
     //const name = this.state.name
-    const { name, counter } = this.state //Atribuição via desestruturação
+    const { posts } = this.state //Atribuição via desestruturação
    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePCLick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            add counter
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
