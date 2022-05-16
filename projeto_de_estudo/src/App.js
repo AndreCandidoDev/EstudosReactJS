96812802e5dee0606f01ee026e65189f01b1b514
andrec762 <andd68@hotmail.com>
2022-05-16 20:03:59 -0300

    metodos de lifecycle

--- projeto_de_estudo/src/App.js
+++ projeto_de_estudo/src/App.js
@@ -5,33 +5,56 @@ class App extends React.Component{
 
   
   state={
-    posts: [
-      {
-        'id':1,
-        'title': 'titulo 1',
-        'body': 'corpo 1'
-      },
-      {
-        'id':2,
-        'title': 'titulo 2',
-        'body': 'corpo 2'
-      },
-      {
-        'id':3,
-        'title': 'titulo 3',
-        'body': 'corpo 3'
-      },
-    ],
+    counter: 0,
+    posts: [{
+      'id':1,
+      'title': 'titulo 1',
+      'body': 'corpo 1'
+    },
+    {
+      'id':2,
+      'title': 'titulo 2',
+      'body': 'corpo 2'
+    },
+    {
+      'id':3,
+      'title': 'titulo 3',
+      'body': 'corpo 3'
+    }],
     name: 'alguem',
     counter:0
   }
 
+  timeoutUpdate = 0
+
+  //executa uma unica vez ao carregar o componente
+  componentDidMount(){
+    this.handleTimeout()
+  }    
+
+  componentDidUpdate(){
+    this.handleTimeout()
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate)
+  }
+    
+  handleTimeout = () => {
+    const { posts, counter } = this.state
+    posts[0].title = 'titulo mudado'
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter:counter+1 })
+    }, 1000);
+  }
+
   render(){
     //const name = this.state.name
-    const { posts } = this.state //Atribuição via desestruturação
+    const { posts, counter } = this.state //Atribuição via desestruturação
    
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
